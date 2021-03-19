5164123d784e540f7f2e278b0ab4a85ddc0d1df3
Andrew Theurer <atheurer@redhat.com>
2021-03-19 14:43:43 -0400

    Initial doc for cyclictest

---
+++ cyclictest/run.sh
@@ -0,0 +1,3 @@
+
+
+crucible run cyclictest --endpoint k8s,host:perf98,user:root,client:1,cpu-partitioning:1,resources:`/bin/pwd`/resource.json,nodeSelector:client-1:`/bin/pwd`/nodeSelector-perf150.json,securityContext:`/bin/pwd`/securityContext.json
